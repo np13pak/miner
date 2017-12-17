4bae1cc91c6f3bcafb7a08d4467339e90feaee1b
np13pak <30809073+np13pak@users.noreply.github.com>
2017-12-17 10:17:51 +0200

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -1,3 +1,3 @@
 #/bin/sh
 #echo "0 */8 * * * /root/miner/sc.sh" > /var/spool/cron/crontabs/root
-echo "0 10 */2 * * /root/miner/sc.sh" > /var/spool/cron/crontabs/root
+#echo "0 10 */2 * * /root/miner/sc.sh" > /var/spool/cron/crontabs/root
